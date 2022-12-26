3364d02d263782263b7376e61ca467e11489eb3c
treepo1 <thiago.freire45@gmail.com>
2022-12-26 14:32:49 -0300

    adicionando cadastro de usuário, padronização de erros e middleware de usuario existente

---
+++ src/errors.mjs
@@ -0,0 +1,9 @@
+const errors = {
+  user: {
+    NOTFOUND: "User was not found!",
+    ALREADYEXISTENT: "User already exists!"
+  }
+}
+
+
+export default errors
--- src/index.js
+++ src/index.js
@@ -1,22 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const errors = require("./errors.mjs");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const req = require('express/lib/request');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    response.status(400).json({error: errors.user.NOTFOUND})
+  }
+
+  request.user = user;
+
+  return next()
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', checksExistsUserAccount, (request, response) => {
+
+  if(request.user) {
+    return response.status(401).json({error:errors.user.ALREADYEXISTENT})
+  }
+
+  const { name, username } = request.body; 
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(newUser);
+
+  response.status(201).json(newUser)
+
 });
+
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
